395fe1ba59d51d7522974e106cf1397fc2faecdc
Egil Eskilsson <egil@egileskilsson.se>
2025-09-10 12:05:48 +0200

    feat(router): add routing and initial page components for JobSearch and JobDetails
    - Created empty JobSearch and JobDetails components
    - Basic router setup

---
+++ src/Router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router";
+import { Layout } from "./pages/layout/Layout";
+import { JobSearch } from "./pages/JobSearch";
+import { JobDetails } from "./pages/JobDetails";
+
+export const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <JobSearch />
+      },
+      {
+        path: '/:id',
+        element: <JobDetails />
+      }
+    ]
+  }
+]);
---
+++ src/pages/JobDetails.tsx
@@ -0,0 +1,7 @@
+export const JobDetails = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
